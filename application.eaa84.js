a81c30d7fa73aac026faadc2566c313c8b7b08e1
b <lehoanghuy11062001@gmail.com>
2025-06-12 11:29:30 +0700

    v5

--- application.eaa84.js
+++ application.eaa84.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.0abb1.json';
+          this.settingsPath = 'src/settings.60f6f.json';
           this.showFPS = false;
         }
 
