a923092a9c12bffa4c85f1fc193407901324d72a
b <lehoanghuy11062001@gmail.com>
2025-06-12 14:01:16 +0700

    v6

--- cocos-js/wait-for-ammo-instantiation.97943.js
+++
@@ -1,1 +0,0 @@
-System.register(["./instantiated-02a0eb3f.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.i9)}],execute:function(){}}}));
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-System.register(["./application.eaa84.js"], function (_export, _context) {
+System.register(["./application.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.eaa84.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationEaa84Js) {
-      Application = _applicationEaa84Js.Application;
+    setters: [function (_applicationJs) {
+      Application = _applicationJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
